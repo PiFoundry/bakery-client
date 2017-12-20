48149b18cc152b252dd9a5c677bee28eff8b15c9
Chris <croeleveld@gmail.com>
2017-12-20 21:35:53 +0100

    add upload method

--- isPiBaked.go
+++ isPiBaked.go
@@ -1,30 +1,12 @@
 package bakeryclient
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-)
-
 func (c *Client) IsPiBaked(piId string) (bool, error) {
-	resp, err := c.httpClient.Get(c.url + "/oven/" + piId)
-	if err != nil {
-		return false, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return false, nil
-	}
-
-	var parsedResponse PiInfo
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(bodyBytes, &parsedResponse)
+	pi, err := c.GetPi(piId)
 	if err != nil {
 		return false, err
 	}
 
-	if parsedResponse.Status != INUSE {
+	if pi.Status != INUSE {
 		return false, nil
 	}
 
